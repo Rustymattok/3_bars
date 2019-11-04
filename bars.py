f2181a4d571720bb52bb07713ed4b68b61b97e08
Rustymattok <you@example.com>
2019-11-04 11:16:49 +0300

    внесены правки в соответствии с pep8

--- bars.py
+++ bars.py
@@ -1,30 +1,37 @@
 import json
 
 
+# 'Parameter for coordinate system.  '
+
+
+POS_X = 37.2
+POS_Y = 55.2
+
+
 # 'This method load json and convert it to dictionary format.  '
 
 
 def load_data(filepath):
-    file = open(filepath)
-    data = json.load(file)
-    return data
+    file_json = open(filepath)
+    bars_list = json.load(file_json)
+    return bars_list
 
 
 # 'This method return the biggest bar(sort by number of seats).  "
 
 
-def get_biggest_bar(data):
-    biggest_bar = max(data["features"],
-                      key=lambda k: k
-                      ["properties"]["Attributes"]["SeatsCount"])
+def get_biggest_bar(bars_list):
+    biggest_bar = max(bars_list["features"],
+                      key=lambda k:
+                      k["properties"]["Attributes"]["SeatsCount"])
     return biggest_bar
 
 
 # 'This method return the smallest bar(sort by number of seats).  "
 
 
-def get_smallest_bar(data):
-    smallest_bar = min(data["features"],
+def get_smallest_bar(bars_list):
+    smallest_bar = min(bars_list["features"],
                        key=lambda k:
                        k["properties"]["Attributes"]["SeatsCount"])
     return smallest_bar
@@ -33,8 +40,8 @@ def get_smallest_bar(data):
 # 'This method return the closest bar.  "
 
 
-def get_closest_bar(data, longitude, latitude):
-    closest_bar = min(data["features"],
+def get_closest_bar(bars_list, longitude, latitude):
+    closest_bar = min(bars_list["features"],
                       key=lambda k:
                       (k["geometry"]['coordinates'][0] - longitude) ** 2 +
                       (k["geometry"]['coordinates'][1] - latitude) ** 2)
@@ -44,4 +51,4 @@ def get_closest_bar(data, longitude, latitude):
 if __name__ == '__main__':
     print(get_biggest_bar(load_data('./bars.json')))
     print(get_smallest_bar(load_data('./bars.json')))
-    print(get_closest_bar(load_data('./bars.json'), 37.2, 55.2))
+    print(get_closest_bar(load_data('./bars.json'), POS_X, POS_Y))
